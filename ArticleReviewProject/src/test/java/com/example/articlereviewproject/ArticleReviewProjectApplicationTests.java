ebee83d9e79088ac952a8f39845831ac8a68874c
Burak Ozgul <nburakozgul@gmail.com>
2022-01-16 12:35:06 +0300

    init

---
+++ ArticleReviewProject/src/test/java/com/example/articlereviewproject/ArticleReviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.articlereviewproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleReviewProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
